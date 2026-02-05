d8651886388b93b749c3cf1ca84bd3b4a8f7a409
work-rjkashyap <work.rjkashyap05@gmail.com>
2026-02-05 21:23:42 +0530

    refactor: remove documentation scripts and unused dependencies from package.json

--- docs/src/app/(home)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { HomeLayout } from 'fumadocs-ui/layouts/home'
-import { baseOptions } from '@/lib/layout.shared'
-
-export default function Layout({ children }: LayoutProps<'/'>): React.ReactElement {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>
-}
