c5d6140ceb78eea1eef27edb266c2204b24d51e3
work-rjkashyap <work.rjkashyap05@gmail.com>
2026-02-10 21:16:57 +0530

    style: format code indentation.

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,27 +1,27 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 import { HyperconnectLogo } from "@/components/logo";
 export function baseOptions(): BaseLayoutProps {
-    return {
-        nav: {
-            title: <HyperconnectLogo style={{ width: 24, height: 24 }} />,
-        },
-        links: [
-            {
-                text: "Documentation",
-                url: "/docs",
-            },
-            {
-                text: "GitHub",
-                url: "https://github.com/work-rjkashyap/Hyper-connect",
-            },
-            {
-                text: "Twitter",
-                url: "https://twitter.com",
-            },
-        ],
-        githubUrl: "https://github.com/work-rjkashyap/Hyper-connect",
-        themeSwitch: {
-            enabled: true,
-        },
-    };
+  return {
+    nav: {
+      title: <HyperconnectLogo style={{ width: 24, height: 24 }} />,
+    },
+    links: [
+      {
+        text: "Documentation",
+        url: "/docs",
+      },
+      {
+        text: "GitHub",
+        url: "https://github.com/work-rjkashyap/Hyper-connect",
+      },
+      {
+        text: "Twitter",
+        url: "https://twitter.com",
+      },
+    ],
+    githubUrl: "https://github.com/work-rjkashyap/Hyper-connect",
+    themeSwitch: {
+      enabled: true,
+    },
+  };
 }
